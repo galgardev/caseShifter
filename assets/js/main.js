21a54ccdd767375ca477557a75f05c94377b739b
Marc Galindo <marc@galgar.dev>
2023-05-20 19:10:35 +0200

    Optimizado main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,16 @@
-// Variables globales
 const body = document.body;
 const themeToggle = document.getElementById('theme-toggle');
 
-// Función para verificar y asignar el tema al cuerpo del documento
 function setTheme() {
     const isDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
     body.className = isDarkMode ? 'dark' : 'light';
 }
 
-// Función para alternar el tema al hacer clic en el botón
 function toggleTheme() {
     body.classList.toggle('dark');
     body.classList.toggle('light');
 }
 
-// Asignar tema inicial al cargar la página
-setTheme(); // Se llama a la función setTheme() al cargar la página
+setTheme();
 
-// Agregar evento de clic al botón para alternar el tema
-themeToggle.addEventListener('click', toggleTheme); // Se llama a la función toggleTheme() al hacer clic en el botón
+themeToggle.addEventListener('click', toggleTheme);
